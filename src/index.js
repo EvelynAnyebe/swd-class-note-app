59bbd686334e543ca219e74cd865c011ce7c0537
Evelyn Anyebe <anyebeevelyn@gmail.com>
2021-07-12 09:28:06 +0100

    Create a PWA

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import './assets/css/index.css';
 
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -14,6 +15,12 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
 
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
